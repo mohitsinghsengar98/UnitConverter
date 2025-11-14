b9f1d439b7686fa346fb1de3e085f73bfefab431
Mohit Singh Sengar <mssengar.988@gmail.com>
2025-11-14 22:18:04 +0530

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Mohit Sengar on 14/11/25.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
